568e46ae3d77210c33058bcb58f5b7128deaff1b
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-20 18:09:56 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCwd4MPdRVnha_yn3Oa-IXumKKdYIs_DzM",
+    authDomain: "arif-time-spot-news.firebaseapp.com",
+    projectId: "arif-time-spot-news",
+    storageBucket: "arif-time-spot-news.appspot.com",
+    messagingSenderId: "19384045130",
+    appId: "1:19384045130:web:5b873e699a3568dfca20b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
